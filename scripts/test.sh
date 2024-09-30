495a9d8ecd8d4701f3ae37a6a7fb2e843bf6aa66
Adam Alton <270255+adamalton@users.noreply.github.com>
2024-09-30 16:01:00 +0100

    Fight the CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,15 @@
-docker run --rm -it \
+
+if [ -z "${CI}" ]; then
+	# Locally case
+	docker run --rm -it \
 	-e DJANGO_SETTINGS_MODULE='test_settings' \
 	-v $PWD/fields_of_gold:/code/fields_of_gold \
 	fields-of-gold python -m django test fields_of_gold
+else
+	# CI case - can't use interactive terminal
+	docker run --rm \
+	-e DJANGO_SETTINGS_MODULE='test_settings' \
+	-v $PWD/fields_of_gold:/code/fields_of_gold \
+	fields-of-gold python -m django test fields_of_gold
+fi
+
